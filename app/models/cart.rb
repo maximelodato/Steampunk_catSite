00633b7d4c3bdb9dbf03835ab50ff28ceca81b5e
michel349u <michel349u@etu.univ-lorraine.fr>
2024-05-29 17:12:26 +0200

    Changement cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,31 +1,37 @@
-
+# app/models/cart.rb
 class Cart
+  attr_reader :items
+
   def initialize(session)
     @session = session
-    @session[:cart] ||= {}
+    @items = session[:cart] || {}
   end
 
   def add_product(product_id)
-    @session[:cart][product_id.to_s] ||= 0
-    @session[:cart][product_id.to_s] += 1
+    product_id = product_id.to_s
+    items[product_id] ||= 0
+    items[product_id] += 1
+    update_session
   end
 
   def remove_product(product_id)
-    @session[:cart].delete(product_id.to_s)
-  end
-
-  def items
-    @session[:cart]
-  end
-
-  def total
-    @session[:cart].values.sum
+    product_id = product_id.to_s
+    if items[product_id]
+      items.delete(product_id)
+      update_session
+    end
   end
 
   def total_price
     items.sum do |product_id, quantity|
-      product = Product.find(product_id)
-      product.price * quantity
+      product = Product.find_by(id: product_id)
+      product ? product.price * quantity : 0
     end
+  end
+
+  private
+
+  def update_session
+    @session[:cart] = items
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Routes pour le panier
   get 'cart', to: 'carts#show', as: 'cart'
   get 'add_to_cart/:product_id', to: 'carts#add_item', as: 'add_to_cart'
-  delete 'remove_from_cart/:product_id', to: 'carts#remove_item', as: 'remove_from_cart'
+  delete 'remove_item', to: 'carts#remove_item', as: 'remove_item'
 
   # Routes pour la liste de souhaits
   get 'wishlist', to: 'wishes#show'
@@ -17,11 +17,11 @@ Rails.application.routes.draw do
   resources :events, only: [:new, :create, :index, :show]
   resources :products, only: [:index, :show]
 
+
   resources :users, only: [:show, :edit, :update]
   # get '/products', to: 'products#index', as: 'products'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
-  get '/cart', to: 'carts#show', as: 'cart'
   # get '/profile', to: 'users#show', as: 'profile'
   get 'payment', to: 'payments#new'
   post 'payment', to: 'payments#create'
