554ec3711a1d3f94b728f7f0b16b1313cc208e47
michel349u <michel349u@etu.univ-lorraine.fr>
2024-05-29 11:48:58 +0200

    Create + update Profil user page + route+ navbar acces

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, only: [:new, :create, :index, :show]
   resources :products, only: [:index, :show]
+  resources :users, only: [:show, :edit, :update]
   # get '/products', to: 'products#index', as: 'products'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/cart', to: 'carts#show', as: 'cart'
-  get '/profile', to: 'users#show', as: 'profile'
+  # get '/profile', to: 'users#show', as: 'profile'
   get 'payment', to: 'payments#new'
   post 'payment', to: 'payments#create'
 end
